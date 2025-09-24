206614c8165add0f81fe5b90745b61ed060ab961
Delaw3 <lauphix1@gmail.com>
2025-09-24 19:13:23 +0100

    Fix: deployment

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,8 @@ export const createApp = () => {
   
 
   // Middleware
-  app.use(express.json({ limit: "1mb" }));
-  app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+  app.use(express.json({ limit: "5mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
   app.use(cors());
   app.use(helmet());
